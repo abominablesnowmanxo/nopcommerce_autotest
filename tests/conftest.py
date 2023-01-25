6bc6d9ba22fb6b4216d3147ae4d7634c10eb8918
Monterey <monterey@Montereys-Mac-mini.local>
2023-01-25 13:23:32 +0400

    headless option added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,13 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 
 def pytest_addoption(parser):
     parser.addoption('--browser', action='store', default='chrome',
                      help='Choose browser: chrome or firefox')
+    parser.addoption('--headless', action='store_true', default=False)
 
 
 @pytest.fixture(scope='function')
@@ -13,13 +15,16 @@ def driver(request):
     browser = request.config.getoption('browser')
     if browser == 'chrome':
         options = ChromeOptions()
-        options.headless = True
+        if request.config.getoption('headless'):
+            options.headless = True
         driver = webdriver.Chrome(options=options)
     elif browser == 'firefox':
-        driver = webdriver.Firefox()
+        options = FirefoxOptions()
+        if request.config.getoption('headless'):
+            options.headless = True
+        driver = webdriver.Firefox(options=options)
     else:
         raise pytest.UsageError('--browser should be chrome or firefox')
     driver.maximize_window()
-    driver.implicitly_wait(10)
     yield driver
     driver.quit()
