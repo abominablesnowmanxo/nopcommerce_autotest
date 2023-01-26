0f2bf145574559eb1a84bc6963df7d9280357219
Андрей Кузнецов <abominablesnowmanxo@gmail.com>
2023-01-26 15:17:20 +0400

    refactoring

--- tests/data/random_data.py
+++ tests/data/random_data.py
@@ -1,8 +1,22 @@
-import string
 import random
+from faker import Faker
+
+from tests.data.user_model import User
 
 
-def random_email():
-    letters = [random.choice(string.ascii_lowercase) for _ in range(0, 7)]
-    rand_email = ''.join(letters) + '@gmail.com'
-    return rand_email
+faker_en = Faker('En')
+
+def generate_user():
+    return User(
+        first_name=faker_en.first_name(),
+        last_name=faker_en.last_name(),
+        email=faker_en.email(),
+        email_mismatch=faker_en.email(),
+        password=faker_en.password(),
+        password_mismatch=faker_en.password(),
+        company=faker_en.company(),
+        day_of_birth=str(random.choice(list(range(1, 32)))),
+        month_of_birth=faker_en.month_name(),
+        year_of_birth=str(random.choice(list(range(1913, 2023)))),
+        gender=random.choice(('Male', 'Female'))
+    )
